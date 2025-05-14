753c1844473da1b3a7ec93ec30dad60ea3726e41
ahmed-bilal-student <ahmedstdbilal0@gmail.com>
2025-05-14 22:14:05 +0500

    Update player.java

--- game/player.java
+++ game/player.java
@@ -1,3 +1,50 @@
-public class player{
-    
+class player {
+    private int x, y;
+    private int speed = 1;
+    private int maxWidth, maxHeight;
+
+    public player(int startX, int startY, int maxWidth, int maxHeight) {
+        this.x = startX;
+        this.y = startY;
+        this.maxWidth = maxWidth;
+        this.maxHeight = maxHeight;
+    }
+
+    public void moveLeft() {
+        if (x - speed >= 0) {
+            x -= speed;
+        }
+    }
+
+    public void moveRight() {
+        if (x + speed < maxWidth) {
+            x += speed;
+        }
+    }
+
+    public void moveUp() {
+        if (y - speed >= 0) {
+            y -= speed;
+        }
+    }
+
+    public void moveDown() {
+        if (y + speed < maxHeight) {
+            y += speed;
+        }
+    }
+
+    public int getX() {
+         return x; 
+    }
+    public int getY() {
+         return y; 
+    }
+
+    @Override
+    public String toString() {
+        return "Player at (" + x + ", " + y + ")";
+    }
 }
+
+
